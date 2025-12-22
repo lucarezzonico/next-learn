dd66bfef4f74d6208d8ba7c9b6ec8f5d3898e951
Luca Rezzonico <RezzoniL@swatchgroup.net>
2025-12-22 14:08:16 +0100

    [Chapter9] Streaming another componnent

--- dashboard/nextjs-dashboard/app/lib/data.ts
+++ dashboard/nextjs-dashboard/app/lib/data.ts
@@ -32,6 +32,12 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -43,6 +49,9 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
+    console.log('Data fetch completed after 3 seconds.');
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
